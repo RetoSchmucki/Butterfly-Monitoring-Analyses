ca3a3c6e3a97a4dac131bc3b38ed47d81570c219
Reto Schmucki <retoschm@ceh.ac.uk>
2024-07-18 17:38:02 +0100

    work with functions

--- .vdoc.r
+++ .vdoc.r
@@ -92,20 +92,28 @@
 #
 #
 #
-#
 #| label: sample-season-missing
-btfl_week_smpl2 <- btfl_week_smpl[month(date) %in% c(4:9), ]
 
-prob_vct <- rep(0.25,btfl_week_smpl2[,.N])
-prob_vct[round(quantile(seq_len(btfl_week_smpl2[,.N]), c(0.20))):round(quantile(seq_len(btfl_week_smpl2[,.N]), c(0.35)))] <- 50
-prob_vct[round(quantile(seq_len(btfl_week_smpl2[,.N]), c(0.65))):round(quantile(seq_len(btfl_week_smpl2[,.N]), c(0.80)))] <- 50
-prob_vct[1:round(quantile(seq_len(btfl_week_smpl2[,.N]), c(0.20)))] <- 0.75
-prob_vct[round(quantile(seq_len(btfl_week_smpl2[,.N]), c(0.80))):btfl_week_smpl2[,.N]] <- 0.75
+sample_missing <- function(data, quantile_breaks = c(0.20, 0.35, 0.65, 0.80), quantile_probs = c(0.25, 0.5, 0.75)){
+      prob_vct <- rep(quantile_probs[1], data[,.N])
+      prob_vct[round(quantile(seq_len(data[,.N]), quantile_breaks[1])):round(quantile(seq_len(data[,.N]), quantile_breaks[2]))] <- quantile_probs[2]
+      prob_vct[round(quantile(seq_len(data[,.N]), quantile_breaks[3])):round(quantile(seq_len(data[,.N]), quantile_breaks[4]))] <- quantile_probs[2]
+      prob_vct[1:round(quantile(seq_len(data[,.N]), quantile_breaks[1]))] <- quantile_probs[3]
+      prob_vct[round(quantile(seq_len(data[,.N]), quantile_breaks[4])):data[,.N]] <- quantile_probs[3]
+      btfl_week_missing <- data[sample(seq_len(.N), round(0.25*.N), prob = prob_vct/sum(prob_vct)), ]
+  return(btfl_week_missing)
+}
 
-btfl_week_missing <- btfl_week_smpl2[sample(seq_len(.N), round(0.25*.N), prob = prob_vct/sum(prob_vct)), ]
+btfl_week_missing <- data.table()
+
+for(i in btfl_week_smpl[, unique(years)]){
+      for(j in btfl_week_smpl[, unique(site_id)]){
+            btfl_week_missing <- rbind(btfl_week_missing, sample_missing(data  = btfl_week_smpl[years == i & site_id == j, ]))
+      }
+}
 
 btfl_fig3 <- ggplot() +
-                geom_point(data=btfl_week_smpl2, aes(x=doy, y=count, colour = "count")) + 
+                geom_point(data=btfl_week_smpl, aes(x=doy, y=count, colour = "count")) + 
                 geom_point(data=btfl_week_missing, aes(x=doy, y=count, colour = "missing"), 
                             shape=4, size=2, stroke=2) + 
                 geom_line(data = btfl_ts,
@@ -116,20 +124,11 @@ btfl_fig3 <- ggplot() +
                       values = c(cnt_col, flc_col, missing_col)) +
                 theme_light() + 
                 theme(legend.position = "inside", legend.position.inside = c(0.9, 0.8)) +
-                labs(title = "Simulated butterfly counts",
+                labs(title = paste0("Simulated butterfly counts (", y,")"),
                      subtitle = "- weekly visit (random resampled)",
                      x = "Day of Year",
                      y = "Count")
 btfl_fig3
-#
-#
-#
-#
-#
-#
-#
-#
-#
 #
 #
 #
